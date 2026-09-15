f70f05151efa45379dc8f034174edb6fe20ca0d5
23dce101-wq <23dce101@charusat.edu.in>
2026-09-15 18:43:25 +0530

    Changes for vercel

--- src/backend/index.js
+++ src/backend/index.js
@@ -24,8 +24,8 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
 
 // index.ts
 var import_config = require("dotenv/config");
-var import_express = __toESM(require("express"), 1);
-var import_cors = __toESM(require("cors"), 1);
+var import_express = __toESM(require("express"));
+var import_cors = __toESM(require("cors"));
 
 // lib/port/schedule.ts
 var HORIZON = 72;
